706eb9ed4bc92553fea1da7094f1840492c5e4b9
Alejandro <alejandro.rodriguez@trackandchain.com>
2022-12-14 18:19:35 +0100

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
