f8bde545a16ff8c03846e54018851e20f50043d6
ispipa <ispipa123@gmail.com>
2022-12-14 20:02:41 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../src/Components/Header/Header';
 import ImagePage from './Components/Img/ImagePage';
+import Bares from './Components/Bares/Bares';
 
 function App() {
   return (
     <>
       <Header />
       <ImagePage />
+      <Bares />
     </>
   );
 }
---
+++ src/Components/Bares/Bares.js
@@ -0,0 +1,84 @@
+import React from "react";
+import './bares.css';
+import  'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function Bares() {
+
+  return (
+    <>
+     <Carousel fade>
+      <Carousel.Item>
+        <CardGroup className="gap-2">
+            <Card className="card2">
+                <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card className="card2">
+                <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card className="card2">
+                <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                </Card.Body>
+            </Card>
+        </CardGroup>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CardGroup className="gap-2">
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CardGroup className="gap-2">
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card className="card2">
+                    <Card.Img variant="top" src="https://images.xceed.me/clubs/covers/bataplan-club-san-sebastian-xceed-605e.jpg?w=28&blur=auto&fm=auto" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+      </Carousel.Item>
+    </Carousel>
+    </>
+  );
+}
+
+export default Bares;
